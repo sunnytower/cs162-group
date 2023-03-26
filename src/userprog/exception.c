803fdd4c2348d62ed6bdc4ce25fc176a03590a97
sunnytower <ljxreal1@gmail.com>
2023-03-26 14:44:27 +0000

    finished checkpoint 2 40/96 failed.

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -78,6 +78,8 @@ static void kill(struct intr_frame* f) {
       printf("%s: dying due to interrupt %#04x (%s).\n", thread_name(), f->vec_no,
              intr_name(f->vec_no));
       intr_dump_frame(f);
+      /* if invalid memory access exit */
+      thread_current()->pcb->exit_status = -1;
       process_exit();
       NOT_REACHED();
 
